0b6fa3d510ef806aedc1926008a3168d71227ea3
algokelvin-373 <kelvinht373@gmail.com>
2021-11-02 18:16:36 +0700

    Create getUserById

--- profile/src/main/java/com/example/profile/MainController.java
+++ profile/src/main/java/com/example/profile/MainController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @Controller
 @RequestMapping(path="/demo")
 public class MainController {
@@ -29,4 +31,11 @@ public class MainController {
         // This returns a JSON or XML with the users
         return peopleRepository.findAll();
     }
+
+    @GetMapping(path = "/profile/{id}")
+    public @ResponseBody
+    Optional<People> getUserById(@PathVariable int id) {
+        return peopleRepository.findById(id);
+    }
+
 }
