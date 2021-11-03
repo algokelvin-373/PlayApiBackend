b0c8939c41ce824ac04a051fbea80d024a3aff4e
algokelvin-373 <kelvinht373@gmail.com>
2021-11-03 18:37:10 +0700

    Success Add Data User

--- profile/src/main/java/com/example/profile/PeopleRepository.java
+++ profile/src/main/java/com/example/profile/PeopleRepository.java
@@ -3,5 +3,5 @@ package com.example.profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PeopleRepository extends JpaRepository<People, Integer> {
+public interface PeopleRepository extends CrudRepository<People, Integer> {
 }
