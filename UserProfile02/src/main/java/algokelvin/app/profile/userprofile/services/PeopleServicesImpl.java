ce3aff5198349632e3aacf37b326e3491759f8a9
algokelvin-373 <kelvinht373@gmail.com>
2022-03-15 20:39:50 +0700

    success build and on progress userprofile 2

---
+++ UserProfile02/src/main/java/algokelvin/app/profile/userprofile/services/PeopleServicesImpl.java
@@ -0,0 +1,10 @@
+package algokelvin.app.profile.userprofile.services;
+
+import algokelvin.app.profile.userprofile.dto.RspPeople;
+
+public class PeopleServicesImpl implements PeopleServices {
+    @Override
+    public RspPeople getProfile(int id) {
+        return null;
+    }
+}
