5fe1662e1cee8bcf2477bd8ad6b13080b71b0590
algokelvin-373 <kelvinht373@gmail.com>
2022-03-16 11:53:27 +0700

    success run Server userhobbies

--- UserProfile02/UserHobbies/src/main/java/algokelvin/app/userhobbies/UserhobbiesApplication.java
+++ UserProfile02/UserHobbies/src/main/java/algokelvin/app/userhobbies/UserhobbiesApplication.java
@@ -1,13 +1,24 @@
 package algokelvin.app.userhobbies;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.boot.SpringApplication.run;
 
 @SpringBootApplication
+@EnableScheduling
+@RestController
 public class UserhobbiesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserhobbiesApplication.class, args);
+		run(UserhobbiesApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String root() {
+		return "Server userhobbies is running...";
 	}
 
 }
