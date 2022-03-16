918f6317b76656328eacbee767ab755703fcd7d6
algokelvin-373 <kelvinht373@gmail.com>
2022-03-16 11:24:50 +0700

    create project userhobbies

---
+++ UserProfile02/UserHobbies/src/main/java/algokelvin/app/userhobbies/UserhobbiesApplication.java
@@ -0,0 +1,13 @@
+package algokelvin.app.userhobbies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserhobbiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserhobbiesApplication.class, args);
+	}
+
+}
---
+++ UserProfile02/UserHobbies/src/test/java/algokelvin/app/userhobbies/UserhobbiesApplicationTests.java
@@ -0,0 +1,13 @@
+package algokelvin.app.userhobbies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserhobbiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
