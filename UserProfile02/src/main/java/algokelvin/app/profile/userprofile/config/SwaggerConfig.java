cb1854faf06ec03fa3af09810f37e973047d3fff
algokelvin-373 <kelvinht373@gmail.com>
2022-03-16 15:34:23 +0700

    create swagger

---
+++ UserProfile02/src/main/java/algokelvin/app/profile/userprofile/config/SwaggerConfig.java
@@ -0,0 +1,67 @@
+package algokelvin.app.profile.userprofile.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/api/v1/**"))
+                .paths(PathSelectors.regex("/error").negate())
+                .build()
+                .apiInfo(metadata())
+                .useDefaultResponseMessages(false)
+                .securitySchemes(Collections.singletonList(apiKey()))
+                .securityContexts(Collections.singletonList(securityContext()))
+                .tags(new Tag("User Auth", "User Authentication Request"))
+                .tags(new Tag("Qr", "Qr request"))
+                .genericModelSubstitutes(Optional.class);
+    }
+
+    private ApiInfo metadata() {
+        return new ApiInfoBuilder()
+                .title("JSON Web Token Authentication API")
+                .description("This is All Documentation for User Profile")
+                .version("1.0.0")
+                .license("MIT License").licenseUrl("http://opensource.org/licenses/MIT")
+                .contact(new Contact(null, null, "kelvinht373@algokelvin.com"))
+                .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+}
